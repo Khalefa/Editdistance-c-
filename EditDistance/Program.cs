63a30f38469bc3f917c74f73bc76650a1c315eb9
basio <khalefam@gmail.com>
2014-06-04 08:44:50 +0200

    passjoin naive

--- EditDistance/Program.cs
+++ EditDistance/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Collections;
 using RTree;
-
+using EditDistance.Passjoin;
 
 namespace EditDistance
 {
@@ -27,12 +27,18 @@ namespace EditDistance
 
         static void Main(string[] args)
         {
-            string []filename = new string[10];
-            filename[0]= @"C:\Users\khalefa\SkyDrive\Alex Work\Work\Edit Distance\datasets\dblpall.format";
-            filename[1] = @"c:\data\tiny.txt";
-             filename[2]=@"c:\data\words.txt";
-            ArrayList words = readinput(filename[2]);
-            Radix.engine.run(words);
+            string[] filename = new string[10];
+            filename[0] = @"C:\Users\khalefa\SkyDrive\Alex Work\Work\Edit Distance\datasets\word.format";
+            filename[1] = @"C:\Users\khalefa\SkyDrive\Alex Work\Work\Edit Distance\datasets\dblpall.format";
+            filename[2] = @"c:\data\tiny.txt";
+            filename[3] = @"c:\data\words.txt";
+
+            ArrayList words = readinput(filename[0]);
+            DateTime t = DateTime.Now;
+            passjoin.compute(words, 1);
+            TimeSpan ts = DateTime.Now - t;
+            Console.Write(ts);
+            //Radix.engine.run(words);
 
         }
     }
