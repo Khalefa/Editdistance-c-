072e8107c61d65b16e095ee72603baf23c8195bc
basio <khalefam@gmail.com>
2014-07-20 11:28:05 +0200

    add some file

---
+++ EditDistance/word.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditDistance
+{
+    class word
+    {
+     public       string w;
+      public  int index;
+    }
+}
